6c074d31e90edf43661b53be0067162e5ba2de44
CCColda <cccolda@gmail.com>
2024-01-02 23:39:03 +0100

    Add HSV to notes

--- data/midi.fragment.glsl
+++ data/midi.fragment.glsl
@@ -5,9 +5,16 @@ uniform float u_cutoff;
 in vec2 p_xy;
 out vec4 o_color;
 
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main() {
 	if (p_xy.y > u_cutoff)
 		discard;
 	else
-		o_color = vec4(1.0, 0.0, 0.0, 1.0);
+		o_color = vec4(hsv2rgb(vec3(p_xy.x - 0.3, 1.0, 1.0)), 1.0);
 }
